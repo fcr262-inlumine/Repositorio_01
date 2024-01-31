470e798753377f6b8397f892a782f66c729ba3d2
Francisco Javier Campos Requena <158211308+fcr262-inlumine@users.noreply.github.com>
2024-01-31 17:18:21 +0100

    Add files via upload

---
+++ Prueba_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]
+  )
+  {
+    int num;
+    printf("Ingrese un entero: ");
+    scanf("%d", &num);
+    printf("Se ingreso un %d", num);
+
+    return 0;
+  }
---
+++ Prueba_02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    cout << "ingrese un entero: ";
+    cin >> num;
+    cout << "Se ingreso un " << num << endl;
+    return 0;
+}
