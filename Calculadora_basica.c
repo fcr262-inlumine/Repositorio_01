cfa738c329153e312bf952e3e089eeaec1d612ff
Francisco Javier Campos Requena <158211308+fcr262-inlumine@users.noreply.github.com>
2024-01-31 18:36:22 +0100

    Update Calculadora_basica.c

--- Calculadora_basica.c
+++ Calculadora_basica.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <conio.h>
+
+int main(){
+  
+  double primer_numero, segundo_numero;
+  char operador;
+  printf("Que desea calcular: ");
+  scan("%lf" "%lf", &primer_numero, &segundo_numero);
+  printf("Elija su operador (+,-,*,/): );
+  scan("%c", &operador);
+
+  switch (operador){
+    case '+';
+    printf("%.5lf + %.5lf = %.5lf", &primer_numero, %segundo_numero, primer_numero + segundo_numero);
+    break;
+    case '-';
+    printf("%.5lf - %.5lf = %.5lf", &primer_numero, %segundo_numero, primer_numero - segundo_numero);
+    break;
+    case '*';
+    printf("%.5lf * %.5lf = %.5lf", &primer_numero, %segundo_numero, primer_numero * segundo_numero);
+    break;
+    case '/';
+    printf("%.5lf / %.5lf = %.5lf", &primer_numero, %segundo_numero, primer_numero / segundo_numero);
+    break;
+    default:
+    printf("Operador externo al programa");
+  }
+  return 0;
+}
 
