0be870a98947c41ede6ff0a2b33305a41373d67e
Francisco Javier Campos Requena <158211308+fcr262-inlumine@users.noreply.github.com>
2024-02-02 13:37:03 +0100

    Update Calculadora_basica.c

--- Calculadora_basica.c
+++ Calculadora_basica.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <conio.h>
 
 int main(){
-  
-  double primer_numero, segundo_numero;
-  char operador;
-  printf("Que desea calcular: ");
-  scan("%lf" "%lf", &primer_numero, &segundo_numero);
-  printf("Elija su operador (+,-,*,/):" );
-  scan("%c", &operador);
-
-  switch (operador){
-    case '+':
-    printf("%.1lf + %.1lf = %.1lf", &primer_numero, &segundo_numero, primer_numero + segundo_numero);
-    break;
-    case '-':
-    printf("%.1lf - %.1lf = %.1lf", &primer_numero, &segundo_numero, primer_numero - segundo_numero);
-    break;
-    case '*':
-    printf("%.1lf * %.1lf = %.1lf", &primer_numero, &segundo_numero, primer_numero * segundo_numero);
-    break;
-    case '/':
-    if(segundo_numero !=0){
-       printf("%.1lf / %.1lf = %.1lf", &primer_numero, &segundo_numero, primer_numero / segundo_numero);
-    }
-      else{
-        printf("No puedo dividir entre 0");
-          }
-      return 1;
-    default:
-    printf("Operador externo al programa");
-  }
-  system("pause");
-  return 0;
+	
+	double first_number, second_number;
+	char operator;
+	
+	
+	printf("Welcome to Calculator =D \n");
+	
+	printf("Enter first number: ");
+	scanf("%lf", &first_number);
+	
+	printf("Enter second number: ");
+	scanf("%lf", &second_number);
+	
+	printf("Enter operator (+,-,*,/): ");
+	scanf(" %c", &operator);
+	
+	switch (operator){
+		case '+':
+			printf("%.5lf %.5lf = %.5lf", first_number, second_number, first_number + second_number);
+		break;
+		case '-':
+			printf("%.5lf %.5lf = %.5lf", first_number, second_number, first_number - second_number);
+		break;
+		case '*':
+			printf("%.5lf %.5lf = %.5lf", first_number, second_number, first_number * second_number);
+		break;
+		case '/':
+			printf("%.5lf %.5lf = %.5lf", first_number, second_number, first_number / second_number);
+		break;
+		default:
+			printf("!ERROR!");
+			
+	}
+	
+	return 0;
 }
-
